2088f70ca2cfaa0942694ab548c6c1b19ac8b1dc
JazzMutant <d-a-ny4@ya.ru>
2020-03-27 21:16:36 +0500

    Split classes namespaces

--- ToDoList.cs
+++ ToDoList.cs
@@ -20,33 +20,34 @@ namespace ToDoList
             bannedUsers = new HashSet<int>();
         }
 
-        public TrackingEntry GeTrackingEntry(int entryId, int userId)
+        public TrackingEntry GetTrackingEntry(int entryId, int userId)
         {
             if (!entries.ContainsKey(entryId))
             {
                 entries[entryId] = new TrackingEntry(entryId);
             }
+
             return entries[entryId];
         }
 
         public void AddEntry(int entryId, int userId, string name, long timestamp)
         {
-            GeTrackingEntry(entryId, userId).AddUpdate(new Update(userId, timestamp, name, null, true));
+            GetTrackingEntry(entryId, userId).AddUpdate(new Update(userId, timestamp, name, null, true));
         }
 
         public void RemoveEntry(int entryId, int userId, long timestamp)
         {
-            GeTrackingEntry(entryId, userId).AddUpdate(new Update(userId, timestamp, null, null, false));
+            GetTrackingEntry(entryId, userId).AddUpdate(new Update(userId, timestamp, null, null, false));
         }
 
         public void MarkDone(int entryId, int userId, long timestamp)
         {
-           GeTrackingEntry(entryId, userId).AddUpdate(new Update(userId, timestamp, null, EntryState.Done, null));
+            GetTrackingEntry(entryId, userId).AddUpdate(new Update(userId, timestamp, null, EntryState.Done, null));
         }
 
         public void MarkUndone(int entryId, int userId, long timestamp)
         {
-            GeTrackingEntry(entryId, userId).AddUpdate(new Update(userId, timestamp, null, EntryState.Undone, null));
+            GetTrackingEntry(entryId, userId).AddUpdate(new Update(userId, timestamp, null, EntryState.Undone, null));
         }
 
         public void DismissUser(int userId)
@@ -67,61 +68,61 @@ namespace ToDoList
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
 
-        public class Update : IComparable<Update>
+    public class Update : IComparable<Update>
+    {
+        public int UserId { get; }
+        public long Timestamp { get; }
+        public string Name { get; }
+        public EntryState? State { get; }
+        public bool? IsVisible { get; }
+
+        public Update(int userId, long timestamp, string newName, EntryState? newState, bool? isVisible)
         {
-            public int UserId { get; }
-            public long Timestamp { get; }
-            public string Name { get; }
-            public EntryState? State { get; }
-            public bool? IsVisible { get; }
-
-            public Update(int userId, long timestamp, string newName, EntryState? newState, bool? isVisible)
-            {
-                UserId = userId;
-                Timestamp = timestamp;
-                Name = newName;
-                State = newState;
-                IsVisible = isVisible;
-            }
-
-            public int CompareTo(Update other)
-            {
-                if (Timestamp != other.Timestamp)
-                    return Timestamp.CompareTo(other.Timestamp);
-
-                if (State != null && other.State != null)
-                    return ((EntryState)other.State).CompareTo((EntryState) State);
-
-                if (IsVisible != null && other.IsVisible != null)
-                    return Name != null && other.Name != null
-                        ? other.UserId.CompareTo(UserId)
-                        : ((bool) other.IsVisible).CompareTo((bool) IsVisible);
-
-                return 1;
-            }
+            UserId = userId;
+            Timestamp = timestamp;
+            Name = newName;
+            State = newState;
+            IsVisible = isVisible;
         }
 
-        public class TrackingEntry
+        public int CompareTo(Update other)
         {
-            private readonly SortedSet<Update> entryUpdates;
-            private readonly int entryId;
+            if (Timestamp != other.Timestamp)
+                return Timestamp.CompareTo(other.Timestamp);
 
-            public TrackingEntry(int entryId)
-            {
-                this.entryId = entryId;
-                entryUpdates = new SortedSet<Update>();
-            }
+            if (State != null && other.State != null)
+                return ((EntryState) other.State).CompareTo((EntryState) State);
 
-            public void AddUpdate(Update update) => entryUpdates.Add(update);
+            if (IsVisible != null && other.IsVisible != null)
+                return Name != null && other.Name != null
+                    ? other.UserId.CompareTo(UserId)
+                    : ((bool) other.IsVisible).CompareTo((bool) IsVisible);
 
-            public Entry GetEntry(HashSet<int> bannedUsers)
-            {
-                var name = entryUpdates.LastOrDefault(x => x.Name != null && !bannedUsers.Contains(x.UserId))?.Name;
-                var state = entryUpdates.LastOrDefault(x => x.State != null && !bannedUsers.Contains(x.UserId))?.State ?? EntryState.Undone;
-                var isVisible = entryUpdates.LastOrDefault(x => x.IsVisible != null && !bannedUsers.Contains(x.UserId))?.IsVisible ?? false;
-                return isVisible ? new Entry(entryId, name, state) : null;
-            }
+            return 1;
+        }
+    }
+
+    public class TrackingEntry
+    {
+        private readonly SortedSet<Update> updates;
+        private readonly int entryId;
+
+        public TrackingEntry(int entryId)
+        {
+            this.entryId = entryId;
+            updates = new SortedSet<Update>();
+        }
+
+        public void AddUpdate(Update update) => updates.Add(update);
+
+        public Entry GetEntry(HashSet<int> bannedUsers)
+        {
+            var name = updates.LastOrDefault(x => x.Name != null && !bannedUsers.Contains(x.UserId))?.Name;
+            var state = updates.LastOrDefault(x => x.State != null && !bannedUsers.Contains(x.UserId))?.State ?? EntryState.Undone;
+            var isVisible = updates.LastOrDefault(x => x.IsVisible != null && !bannedUsers.Contains(x.UserId))?.IsVisible ?? false;
+            return isVisible ? new Entry(entryId, name, state) : null;
         }
     }
 }
